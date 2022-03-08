705c581f665a2e321b29b95147936f71eed8ba76
gayu2510 <90849745+gayu2510@users.noreply.github.com>
2022-03-08 11:55:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ def yaml_dump(filepath, data):
 
 file_path = "DataSet/Milestone1/Milestone1A.yaml"
 data = yaml_loader(file_path)
-print(data)
+print(data['M1A_Workflow'].items())
+for i in data['M1A_Workflow'].items():
+    if i[0] == 'Activities':
+        for ta in data['M1A_Workflow'][i[0]].items():
+            if ta[1]['Type'] == 'Task':
+                print(ta[0],end=" ")
+                print('entered')
